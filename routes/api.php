013e801edbfddbb175959a03e04d521e606aa705
Ana Claudia <anacnogueira@gmail.com>
2024-09-05 15:21:55 -0300

    Cria rota e controller de wallet

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WalletController;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('wallets', WalletController::class);
