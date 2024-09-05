64efeb05bbeab75d19ae3742f62ed23d373e529d
Ana Claudia <anacnogueira@gmail.com>
2024-09-05 11:45:12 -0300

    Lista usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\UserService;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+    
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $users = $this->userService->getAllUsers();
+
+        return UserResource::collection($users);
     }
 
     /**
